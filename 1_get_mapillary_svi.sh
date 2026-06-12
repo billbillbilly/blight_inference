9d3c5c6340833a6fc477b0c8e4f647fa1fc0bfa4
Xiaohao Yang <61219306+billbillbilly@users.noreply.github.com>
2026-06-12 00:57:27 -0400

    Add script to process Mapillary SVI data in chunks
    Script calculates start and end values for processing chunks of data and invokes a Python script.

---
+++ 1_get_mapillary_svi.sh
@@ -0,0 +1,12 @@
+TOTAL=236107
+CHUNK=5000
+
+START=$((1 * CHUNK))
+END=$((START + CHUNK))
+if [ "$END" -gt "$TOTAL" ]; then
+  END="$TOTAL"
+fi
+
+echo "Task ${TASK_ID}: processing ${START}..${END}"
+
+python svi.py --start "$START" --end "$END"
